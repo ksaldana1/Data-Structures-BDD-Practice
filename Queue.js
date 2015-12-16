2cc8e7c9a8bb2eaef3f218d5b44c3d8f21ac65e5
ksaldana1 <ksaldana1@gmail.com>
2015-12-16 13:23:47 -0600

    queue comments added

--- Queue.js
+++ Queue.js
@@ -1,27 +1,36 @@
 /*! Queue v1.0.0 - MIT license */
 'use strict';
+// Queue constructor
 var Queue = function() {
   this._storage = {};
   this._start = -1;
   this._end = -1;
 };
 
+// Add value to queue
 Queue.prototype.enqueue = function(val) {
+  // add to end property (first value will be this._storage[0])
   this._end++;
   this._storage[this._end] = val;
+  // return value passed into function
   return val;
 };
 
 Queue.prototype.dequeue = function() {
+  // if queue is empty, return
   if (this.size() === 0) {
     return;
   }
+  // increase start property (first value will be this._storage[0])
   this._start++;
+  // assign to temp variable to return
   var temp = this._storage[this._start];
+  // remove from queue
   delete this._storage[this._start];
   return temp;
 };
 
 Queue.prototype.size = function() {
+  // calculate number of items in queue
   return this._end - this._start;
 };
