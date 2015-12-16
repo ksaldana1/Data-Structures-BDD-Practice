c037d032ea39298eb2aee4e93b828458e1336cad
ksaldana1 <ksaldana1@gmail.com>
2015-12-16 13:50:33 -0600

    stack tests written & passing

--- js/Stack.js
+++ js/Stack.js
@@ -1,4 +1,30 @@
 /*! Stack v1.0.0 - MIT license */
+'use strict';
+// Stack constructor
 var Stack = function() {
   this._storage = {};
-}
+  this._size = 0;
+};
+
+// add value to stack
+Stack.prototype.push = function(val) {
+  this._size++;
+  this._storage[this._size] = val;
+};
+
+// remove value from stack --> return removed value
+Stack.prototype.pop = function() {
+  // check if anything is in stack
+  if (this.size() === 0) {
+    return;
+  }
+  var temp = this._storage[this._size];
+  delete this._storage[this._size];
+  this._size--;
+  return temp;
+};
+
+// getter function for stack size property
+Stack.prototype.size = function() {
+  return this._size;
+};
--- spec/Stack.js
+++ spec/Stack.js
@@ -10,12 +10,55 @@ describe('Stack()', function() {
     expect(stack).to.be.a('object');
   });
 
-  it('should do stuff', function() {
-    expect(true).to.equal(false);
+  it('should do have methods: push, pop, size', function() {
+    expect(stack.push).to.be.a('function');
+    expect(stack.pop).to.be.a('function');
+    expect(stack.size).to.be.a('function');
   });
 
-  it('should do more stuff', function() {
-    expect(true).to.equal(false);
+  it('should report size of zero for new stack', function() {
+    expect(stack.size()).to.equal(0);
   });
+
+  it('should report a size of two after adding two items', function() {
+    stack.push('a');
+    stack.push('b');
+    expect(stack.size()).to.equal(2);
+  });
+
+  it('should report a size of one after adding two items then removing one', function() {
+    stack.push('a');
+    stack.push('b');
+    stack.pop();
+    expect(stack.size()).to.equal(1);
+  });
+
+  it('should return a size of 0 if more items removed than added', function() {
+    stack.push('a');
+    stack.pop();
+    stack.pop();
+    expect(stack.size()).to.equal(0);
+  });
+
+  it('should remove most recently added item', function() {
+    stack.push('a');
+    stack.push('b');
+    expect(stack.pop()).to.equal('b');
+  });
+
+  it('should allow for sequential add/remove', function() {
+    stack.push('a');
+    expect(stack.pop()).to.equal('a');
+    stack.push('b');
+    expect(stack.pop()).to.equal('b');
+  });
+
+  it('removes newest after newer added and removed', function() {
+    stack.push('a');
+    stack.push('b');
+    stack.push('c');
+    stack.pop();
+    expect(stack.pop()).to.equal('b');
+  })
 
 });
