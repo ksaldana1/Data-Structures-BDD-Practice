e108f149e8feece60e1107fe60a637f6af7d89ad
ksaldana1 <ksaldana1@gmail.com>
2015-12-17 15:47:40 -0600

    tree spec format started

---
+++ js/Tree.js
@@ -0,0 +1,6 @@
+/*! Tree v1.0.0 - MIT license */
+'use strict';
+
+var Tree = function() {
+  this.children = [];
+};
---
+++ spec/Tree.js
@@ -0,0 +1,22 @@
+/* global Tree, describe, it, expect, should */
+
+describe('Tree()', function() {
+  var tree;
+  beforeEach(function() {
+    tree = new Tree();
+  });
+
+  it('should make a new tree', function() {
+    expect(true).to.equal(false);
+  });
+
+  it('should do some other stuff', function() {
+    expect(true).to.equal(false);
+  });
+
+  it('should do some even more stuff', function() {
+    expect(true).to.equal(false);
+  });
+
+
+});
